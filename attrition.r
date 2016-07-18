e62e6ec3893739ec8f1379fe68d43a2b696a5fd7
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-07-18 18:23:38 -0400

    Built all retention plot. Added on width based on number of teams to highlight regions with stupid high retention but only 1 team (i.e. Delaware)

--- attrition.r
+++ attrition.r
@@ -5,6 +5,12 @@ library(gdata)
 
 team_data_path <- "data/teams.csv"
 
+lost_teams = function(teams, year){
+  lost <- setdiff(filter(teams, Year==year-1)$X., filter(teams, Year==year)$X.)
+  lost_team_ages <- teams[teams$X. %in% lost, ]
+  group_by(lost_team_ages, X.) %>% summarize(age=n())
+}
+
 # Compute percentage loss for teams
 lost_percent <- function(teams, year)
   length(setdiff(filter(teams, Year==year-1)$X., filter(teams, Year==year)$X.))/length(filter(teams, Year==year)$X.)
@@ -21,7 +27,8 @@ years <- seq(2005, 2016)
 
 compute_losses <- function(teams_sub, factor="ALL")
   data.frame(factor=factor, years=years, 'attrition pct'=sapply(years, function(year) 
-    100-lost_percent(teams_sub, year)*100)
+    100-lost_percent(teams_sub, year)*100), 'team count'=sapply(years, function(year) 
+      length(filter(teams_sub, Year==year)$X.))
   )
 
 
@@ -30,11 +37,12 @@ districts <- rbind(
   compute_losses(filter(teams, Locale=='DE' | Locale=='NJ' | Locale=='PA'), 'MAR'),
   compute_losses(filter(teams, Locale=='CT' | Locale=='MA' | Locale=='ME' | Locale=='VT' | Locale=='NH'), 'NE'),
   compute_losses(filter(teams, Locale=='MI'), 'MI'),
-  #compute_losses(filter(teams, Locale=='IN'), 'IN'),
+  compute_losses(filter(teams, Locale=='IN'), 'IN'),
   compute_losses(filter(teams, Locale=='MD' | Locale=='VA' | Locale=='DC'), 'CHS'),
-  #compute_losses(filter(teams, Locale=='GA'), 'GA'),
-  #compute_losses(filter(teams, Locale=='NC'), 'NC'),
-  compute_losses(teams, 'ALL')
+  compute_losses(filter(teams, Locale=='GA'), 'GA'),
+  compute_losses(filter(teams, Locale=='MN'), 'MN'),
+  compute_losses(filter(teams, Locale=='NC'), 'NC')
+  #compute_losses(teams, 'ALL')
 )
 
 p <- ggplot(districts, mapping = aes(years,attrition.pct, col=factor))
@@ -43,17 +51,17 @@ p <- ggplot(districts, mapping = aes(years,attrition.pct, col=factor))
 retention<- p + geom_line(size=1.06, linejoin="mitre") + scale_x_continuous(breaks = years) + scale_y_continuous(breaks=seq(0,100)) + theme_fivethirtyeight() + scale_color_tableau()+ xlab(label = "Year") +ylab(label="Retention %") + theme(legend.title=element_blank())
 
 
-ggsave("images/district_retention", plot=retention, height=5, width=10)
+ggsave("images/district_retention.png", plot=retention, height=5, width=10)
 
 anno <- read.csv("data/annotations.csv")
 
 retention + annotate("text", x=anno$year, y=anno$y, label=anno$label, size=3, hjust=-0.05) + annotate("point", x=anno$year, y=anno$y, shape=21) 
-ggsave("images/annotated_district_retention.png", height=5, width=10,)
+ggsave("images/annotated_district_retention.png", height=5, width=10)
 
 
 m <- ggplot(districts, mapping = aes(years,attrition.pct, group=factor))
 michigan <-m + geom_line(size=1, linejoin="mitre", color="gray", alpha=0.4) + scale_x_continuous(breaks = years) + scale_y_continuous(breaks=seq(0,100)) + theme_fivethirtyeight() + scale_color_tableau()+ xlab(label = "Year") +ylab(label="Retention %") + theme(legend.title=element_blank()) + ylim(80,100) + geom_line(data=filter(districts, factor=='MI'), color="orange", size=1.06)
-ggsave("images/michigan_retention.png", height=5, width=10,)
+ggsave("images/michigan_retention.png", height=5, width=10)
 
 
 
@@ -68,9 +76,16 @@ for(locale in regions){
 }
 
 
+losses$lower = losses$attrition.pct -  min*scale(losses$team.count, center = 0)
+losses$upper = losses$attrition.pct +  min*scale(losses$team.count, center = 0)
+min = 5
 
-retention <- losses[c("factor", "years", "attrition.pct")]
-colnames(retention) <- c("region", "year", "pct_retained")
+ggplot(losses[losses$factor!='ALL',]) + geom_ribbon(mapping=aes(ymin=lower, ymax=upper, x=years, y=attrition.pct, group=factor), fill="orange") + facet_wrap(facets="factor") + theme_fivethirtyeight() + scale_color_tableau()+ xlab(label = "Year") +ylab(label="Retention %") + theme(legend.title=element_blank()) + coord_cartesian(ylim=c(80,100)) + geom_line(mapping=aes(years, attrition.pct), col="black", alpha=0.5)
+ggsave("images/all_retention_with_teams.png", height=12, width=12)
+
+
+retention <- losses[c("factor", "years", "attrition.pct", "team.count")]
+colnames(retention) <- c("region", "year", "pct_retained", "teams")
 write.csv(retention, file = "data/retention.csv")
 
 
